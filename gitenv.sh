8e34bafdb5f7c99b6f62a718dd4b275994179397
Miroslav Cibulka <miroslav.cibulka@flowup.cz>
2020-02-26 22:36:48 +0100

    feat(push-feature-for): added new flow when <BRANCH>-<ENV> already exists
    - updates it with reset --hard
    - pushes it with push --force if update happened

--- gitenv.sh
+++ gitenv.sh
@@ -96,11 +96,21 @@ elif [[ "$1" == "push-feature-for" ]]; then
 
     TARGET_BRANCH=$2
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    PUSH_FLAGS=
 
     run git fetch --prune
-    run git checkout -b ${BRANCH}-${TARGET_BRANCH}
+    if ! git show-ref --verify --quiet refs/heads/${BRANCH}-${TARGET_BRANCH}; then
+        echo "Branch ${BRANCH}-${TARGET_BRANCH} doesn't exists, creating new one"
+        run git checkout -b ${BRANCH}-${TARGET_BRANCH}
+    else
+        echo "Branch ${BRANCH}-${TARGET_BRANCH} exists, moving everything there from branch ${BRANCH}"
+        run git checkout ${BRANCH}-${TARGET_BRANCH}
+        run git reset --hard ${BRANCH}
+        PUSH_FLAGS="${PUSH_FLAGS} --force"
+    fi
+
     run git pull --rebase origin ${TARGET_BRANCH}
-    run git push origin ${BRANCH}-${TARGET_BRANCH}
+    run git push origin ${BRANCH}-${TARGET_BRANCH} ${PUSH_FLAGS};
     run git checkout ${BRANCH}
 
     exit 0
